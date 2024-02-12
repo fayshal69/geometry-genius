20e3f0779213efbb93939a27666d3eac9d5f5868
fayshal-bin-amir <foyshalbinamir@gmail.com>
2024-02-12 15:14:46 +0600

    added parallelogram calculate function

--- app.js
+++ app.js
@@ -26,9 +26,33 @@ document.getElementById('triangleCalculation').addEventListener('click', functio
 });
 
 // rectangle calculate function
-document.getElementById('triangleCalculation').addEventListener('click', function() {
-    const b = document.getElementById('triangleB');
-    const h = document.getElementById('triangleH');
+document.getElementById('rectangleCalculation').addEventListener('click', function() {
+    const w = document.getElementById('rectangleW');
+    const l = document.getElementById('rectangleL');
+    const wValue = w.value;
+    const lValue = l.value;
+
+    if(wValue.trim() === '' || lValue.trim() === '') {
+        return;
+    }
+    if(!isNaN(wValue) && !isNaN(lValue)) {
+        const result = (wValue * lValue);
+        const rectangleResult = document.getElementById('rectangleResult');
+        rectangleResult.innerText = result;
+        w.value = '';
+        l.value = '';
+        count += 1;
+        const p = document.createElement('p');
+        p.innerText = `${count}. Rectangle Area = ${result} cm2`;
+        historyContainer.appendChild(p);
+    }
+    return;
+});
+
+// Parallelogram calculate function
+document.getElementById('parallelogramCalculation').addEventListener('click', function() {
+    const b = document.getElementById('parallelogramB');
+    const h = document.getElementById('parallelogramH');
     const bValue = b.value;
     const hValue = h.value;
 
@@ -36,14 +60,14 @@ document.getElementById('triangleCalculation').addEventListener('click', functio
         return;
     }
     if(!isNaN(bValue) && !isNaN(hValue)) {
-        const result = (bValue * hValue) / 2;
-        const triangleResult = document.getElementById('triangleResult');
-        triangleResult.innerText = result;
+        const result = (bValue * hValue);
+        const parallelogramResult = document.getElementById('parallelogramResult');
+        parallelogramResult.innerText = result;
         b.value = '';
         h.value = '';
         count += 1;
         const p = document.createElement('p');
-        p.innerText = `${count}. Triangle Area = ${result} cm2`;
+        p.innerText = `${count}. Parallelogram Area = ${result} cm2`;
         historyContainer.appendChild(p);
     }
     return;
